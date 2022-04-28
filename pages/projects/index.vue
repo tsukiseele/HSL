292dd7267407cffe25e564fa4e64ab13166ccd9e
wangjun <tsukiseele@hotmail.com>
2022-04-28 18:38:33 +0800

    Update index.vue

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -2,9 +2,11 @@
 #content
   .projects 
     .project-item(v-for="project in projects" :key="project.name" data-aos="fade-up") 
-      .project-name {{ project.name }}
-      .project-desc {{ project.desc }}
-      img.project-cover(:src="project.preview" :alt="project.name")
+      .project-cover-wrapper
+        img.project-cover(:src="project.preview" :alt="project.name")
+      .project-description
+        .project-name {{ project.name }}
+        .project-desc {{ project.desc }}
 
 </template>
 
@@ -15,30 +17,55 @@ export default {
       {
         name: 'MoeViewerR',
         desc: 'lorem',
-        preview: ''
+        preview: 'http://placekitten.com/300/300',
       },
       {
-        
         name: 'MoeViewerR',
         desc: 'lorem',
-        preview: ''
-      }
-    ]
+        preview: 'http://placekitten.com/300/300',
+      },
+    ],
   }),
-  methods: {
-    
-  }
+  methods: {},
 }
 </script>
 <style lang="scss" scoped>
 #content {
-  margin-top: calc(-1 * var(--nav-height));
-  
-    background-color: #77428D;
+  // margin-top: calc(-1 * var(--nav-height));
+
+  // background-color: #77428d;
   .projects {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
     .project-item {
       margin: 0;
-      height: 100vh;
+      width: 0;
+      flex: 0 0 280px;
+      border-radius: 5px;
+      box-shadow: var(--shadow);
+      margin: 1rem;
+
+      background-color: rgba($color: white, $alpha: 0.33);
+      backdrop-filter: blur(16px);
+      overflow: hidden;
+
+      .project-cover-wrapper {
+        width: 280px;
+        height: 280px;
+        overflow: hidden;
+        padding: 0.5rem;
+        img {
+          width: 100%;
+          height: 100%;
+
+          object-fit: cover;
+        }
+      }
+      .project-description {
+        padding: .5rem;
+      }
     }
   }
 }
