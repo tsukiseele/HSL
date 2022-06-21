e4f4029659a0c11d7d6996b20a87787a4811e299
tsukiseele <tsukiseele@hotmail.com>
2022-06-21 14:42:10 +0800

    update

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
 #content
   .about.card
+    .title  
+      span.letter__wrapper(v-for="char in name.toLowerCase()" )
+        .letter(:class="char")
     .blockquote
       s-icon(name='mdi-format-quote-open').quote-left
       .quote-content 砂糖少女は愛を食む
@@ -24,6 +27,7 @@ export default {
     })
   },
   data: () => ({
+      name: 'tsukiseele',
     header: {
       title: '关于',
       subtitle: '',
